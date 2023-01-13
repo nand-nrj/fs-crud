00bc0da437bb4b87fe38057725a198e406534532
Nand Raj <76772181+nandcoder@users.noreply.github.com>
2023-01-13 22:46:15 +0530

    minor update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use('/files', files)
 app.get('/', (req, res) => {
     res.write("<h1>Welcome to File System CRUD API</h1>")
-    res.send`<h3>For details about the endpoints - <a href="https://github.com/nandcoder/fs-crud"> Read the Docs </a> </h3>`)
+    res.send(`<h3>For details about the endpoints - <a href="https://github.com/nandcoder/fs-crud"> Read the Docs </a> </h3>`)
 })
 
 app.listen(3000, () => {
